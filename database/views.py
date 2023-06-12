0da72026db3eba95dd27c22a2f08b251bf372854
Jean-Luc <98668799+JohnSite07@users.noreply.github.com>
2023-06-12 11:30:15 +0100

    Update views.py

--- database/views.py
+++ database/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import DataQueryForm
 
 def resume(request):
     return render(request, 'database/resume.html')
+
+def dataquery(request):
+    dq_form = DataQueryForm()
+
+    context = {
+        "dq_form": dq_form
+    }
+    return render(request, "database/request_data.html", context)
