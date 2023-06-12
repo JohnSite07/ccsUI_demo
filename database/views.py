95029cfcd4c26d3e5a698707ab4f2af4779fd1ab
Jean-Luc <98668799+JohnSite07@users.noreply.github.com>
2023-06-12 12:12:31 +0100

    Update views.py

--- database/views.py
+++ database/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from .forms import DataQueryForm
+from .variables import *
 
 def resume(request):
     return render(request, 'database/resume.html')
 
 def dataquery(request):
-    dq_form = DataQueryForm()
+    import psycopg2 as psyco
+
+    conn = psyco.connect(
+        host = host,
+        database = database,
+        user = user,
+        password = password
+    )
+
+    if request.method == 'POST':
+        dq_form = DataQueryForm(request.POST)
+        if dq_form.is_valid():
+            tableau = dq_form.cleaned_data['tableau']
+            table = select(tableau, conn).to_html(index=False,
+                                classes=[
+                                    "table table-secondary table-hover",
+                                    ]
+                                    )
+            return render(request, "database/data_requested.html", {"table":table})
+    else:
+        dq_form = DataQueryForm()
 
     context = {
         "dq_form": dq_form
